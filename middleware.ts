70ba282d3331cb3a192357b354a0bb8fd331d27c
jjeganvijay <jjeganvijay@gmail.com>
2024-08-20 19:09:34 +0530

    first commit

--- middleware.ts
+++ middleware.ts
@@ -13,6 +13,15 @@ export default clerkMiddleware((auth, req) => {
   if (protectedRoute(req)) auth().protect();
 });
 
+
+
+
+
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-};
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
